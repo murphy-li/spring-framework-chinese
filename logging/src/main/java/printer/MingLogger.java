975bf31f04970870c6af58f4aee8d4b9d60e218f
murphy-li <murphy-li@foxmail.com>
2020-03-10 21:30:03 +0800

    添加日志类

---
+++ logging/src/main/java/printer/MingLogger.java
@@ -0,0 +1,12 @@
+package printer;
+
+import java.util.logging.Logger;
+
+/**
+ * @author 17326
+ * @date 2020/3/5 0:33
+ */
+public interface MingLogger {
+	void info(String msg);
+	void error(String msg);
+}
--- logging/src/main/java/printer/MingLoggerImpl.java
+++ logging/src/main/java/printer/MingLoggerImpl.java
@@ -6,11 +6,15 @@ package printer;
  * @author 17326
  * @date 2020/2/27 22:24
  */
-public class DebugPrinter {
+public class MingLoggerImpl implements MingLogger{
+	String name;
+	public MingLoggerImpl(String name) {
+		this.name = name;
+	}
+
 	/**
 	 * 打印调用方的信息
 	 */
-	static int i = 1;
 	public static void log(String remark){
 		Throwable throwable = new Exception();
 		// 需要打印的方法所在的类
@@ -26,12 +30,43 @@ public class DebugPrinter {
 		String callerMethodName = throwable.getStackTrace()[2].getMethodName();
 		int callerLineNum = throwable.getStackTrace()[2].getLineNumber();
 //		logger.info("调用者：" + callerClassName + "#" + callerMethodName + ":" + callerLineNum + "==》被调用者：" + className + "#" +methodName + lineNum + "==》备注：" + remark);
-		System.out.println("调用者：" + callerClassName + "#" + callerMethodName + ":" + callerLineNum + "\n被调用者：" + className + "#" +methodName + lineNum + "\n备注：" + remark +"\n");
+//		System.out.println("调用者：" + callerClassName + "#" + callerMethodName + ":" + callerLineNum + "\n被调用者：" + className + "#" +methodName + lineNum + "\n备注：" + remark +"\n");
 //		System.out.println(  callerClassName + "#" + callerMethodName + ":" + callerLineNum + "\n" + className + "#" +methodName + lineNum + "\n" + remark +"\n");
 //		logger.info(className + "#" +methodName + lineNum + "==》备注：" + remark);
 //		System.out.println(className + "#" +methodName + lineNum + " 备注：" + remark);
-
 	}
+
+	public static MingLogger getLogger(String name) {
+		return new MingLoggerImpl(name);
+	}
+
+	public static <T> MingLogger getLogger(Class<T> clazz) {
+		return new MingLoggerImpl(clazz.getName());
+	}
+
+	private String getMsgContent(String msg){
+
+		Throwable throwable = new Exception();
+		// 需要打印的方法所在的类
+		String className = throwable.getStackTrace()[2].getClassName();
+		className = className.substring(className.lastIndexOf(".") + 1);
+		// 需要打印的调用方方法名
+		String methodName = throwable.getStackTrace()[2].getMethodName();
+		int lineNum = throwable.getStackTrace()[2].getLineNumber();
+		return className + "#" +methodName + ":" + lineNum + ":" + msg;
+	}
+	@Override
+	public void info(String msg) {
+		String output = getMsgContent(msg);
+		System.out.println(output);
+	}
+
+	@Override
+	public void error(String msg) {
+		String output = getMsgContent(msg);
+		System.err.println(output);
+	}
+
 	public static void log() {
 		log("");
 	}
